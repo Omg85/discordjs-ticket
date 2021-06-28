232f1daa477702ef5b7cf084837bf279519d368a
d̶a̶r̶k̶ b̶o̶t̶ <79138686+Omg85@users.noreply.github.com>
2021-06-28 19:47:11 +0800

    Create index.d.ts

---
+++ typings/index.d.ts
@@ -0,0 +1,21 @@
+declare module 'discordjs-ticket' {
+    import Discord from 'discord.js';
+
+    declare class Ticket {
+        emit(event: string | symbol, ...args: any[]): boolean;
+
+        on(event: string | symbol, listener: (...args: any[]) => void): void;
+        on(event: 'close', listener: () => void): void;
+        on(event: 'create', listener: (channel: Discord.GuildChannel, user: Discord.User) => any): void;
+
+        once(event: string | symbol, listener: (...args: any[]) => void): void;
+        once(event: 'close', listener: () => void): void;
+        once(event: 'create', listener: (channel: Discord.GuildChannel, user: Discord.User) => void): void;
+
+        setTicketChannel(channel: Discord.GuildChannel, embedOpt: embedOpt): Promise<void>;
+        createTicketChannel(guild: Discord.Guild, user: Discord.User): Promise<void>;
+        closeTicket(channel: Discord.GuildChannel): void;
+    }
+
+    export = Ticket;
+}
